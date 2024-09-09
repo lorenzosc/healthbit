6fb90ca032d2bf79d382e98732218033dd63a101
lorenzosc <lorenzocorrea6@hotmail.com>
2024-09-09 01:27:55 -0300

    Added option files

---
+++ src/options/ConditionOptions.js
@@ -0,0 +1,29 @@
+export const conditions = [
+    { name: 'diabetes', label: 'Diabetes' },
+    { name: 'cardiacProblems', label: 'Problemas Cardíacos' },
+    { name: 'highBloodPressure', label: 'Pressão Alta' },
+    { name: 'asthma', label: 'Asma' },
+    { name: 'depression', label: 'Depressão' },
+    { name: 'anxiety', label: 'Ansiedade' },
+    { name: 'highCholesterol', label: 'Colesterol Alto' },
+    { name: 'backPain', label: 'Dores nas Costas' },
+    { name: 'jointPain', label: 'Dores nas Articulações' },
+    { name: 'headache', label: 'Dores de Cabeça' },
+    { name: 'cancer', label: 'Câncer' },
+    { name: 'stds', label: 'Infecções Sexualmente Transmissíveis' },
+];
+
+export const conditionAlreadyHad = [
+    'cardiacProblems',
+    'backPain',
+    'jointPain',
+    'headache',
+    'cancer',
+    'stds'
+]
+
+export const conditionNotKnow = [
+    'diabetes',
+    'highBloodPressure',
+    'highCholesterol'
+]
