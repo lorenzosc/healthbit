ab3bcd57aa8a49c7405f736669a3d1c6599d5468
lorenzosc <lorenzocorrea6@hotmail.com>
2024-09-09 01:27:43 -0300

    Changed radio-group to remember choices

--- src/components/RadioGroup.js
+++ src/components/RadioGroup.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const RadioGroup = ({ label, name, options, onChange }) => (
+const RadioGroup = ({ label, name, options, onChange, selectedValue }) => (
   <div className="form-group">
     <label>{label}</label>
     <div className="radio-group">
       {options.map((option, index) => (
-        <label key={index}>
-          <input type="radio" name={name} value={option.value} onChange={onChange} /> {option.label}
-        </label>
+        <div key={index}>
+          <label>
+            <input type="radio" name={name} value={option.value} onChange={onChange} checked={selectedValue === option.value}/> {option.label} 
+          </label>
+        </div>
       ))}
     </div>
   </div>
